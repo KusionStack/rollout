56b117d39c5126d8f705f9d44f6241adad4ec409
zoumo <jim.zoumo@gmail.com>
2023-12-13 18:19:21 +0800

    feat: delete fastBatch in batchStrategy

--- pkg/controllers/rollout/upgrade_workflow.go
+++ pkg/controllers/rollout/upgrade_workflow.go
@@ -15,13 +15,7 @@
 package rollout
 
 import (
-	"fmt"
-	"math"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/utils/pointer"
 
 	"kusionstack.io/rollout/apis/rollout"
 	rolloutv1alpha1 "kusionstack.io/rollout/apis/rollout/v1alpha1"
@@ -29,55 +23,6 @@ import (
 	"kusionstack.io/rollout/pkg/features/ontimestrategy"
 	"kusionstack.io/rollout/pkg/workload"
 )
-
-// func constructBatches(strategy *rolloutv1alpha1.RolloutStrategy, workloadWrappers []workload.Interface) []types.BatchDetail {
-// 	batches := strategy.Batch.Batches
-// 	if strategy.Batch.FastBatch != nil {
-// 		batches = convertFastBatch(strategy.Batch.FastBatch)
-// 	}
-
-// 	var failureThreshold *intstr.IntOrString
-// 	var initialDelaySeconds int32
-
-// 	if strategy.Batch.Toleration != nil {
-// 		failureThreshold = strategy.Batch.Toleration.WorkloadFailureThreshold
-// 		initialDelaySeconds = strategy.Batch.Toleration.InitialDelaySeconds
-// 	}
-
-// 	result := make([]types.BatchDetail, 0)
-// 	for i, b := range batches {
-// 		workloadTasks := make(map[string]*types.WorkloadBatchTask)
-
-// 		filteredWorkloads := filterWorkloadsByMatch(workloadWrappers, b.Match)
-// 		for _, w := range filteredWorkloads {
-// 			wkey := fmt.Sprintf("%s/%s", w.GetCluster(), w.GetObj().GetName())
-// 			task := &types.WorkloadBatchTask{
-// 				Workload:            w,
-// 				Replicas:            b.Replicas,
-// 				FailureThreshold:    failureThreshold,
-// 				InitialDelaySeconds: initialDelaySeconds,
-// 			}
-// 			workloadTasks[wkey] = task
-// 		}
-
-// 		tasks := []types.WorkloadBatchTask{}
-// 		for _, task := range workloadTasks {
-// 			tasks = append(tasks, *task)
-// 		}
-
-// 		batch := types.BatchDetail{
-// 			Index:         i,
-// 			WorkloadTasks: tasks,
-// 		}
-
-// 		if b.Pause != nil && *b.Pause {
-// 			batch.NeedPause = true
-// 		}
-
-// 		result = append(result, batch)
-// 	}
-// 	return result
-// }
 
 func filterWorkloadsByMatch(workloads []workload.Interface, match *rolloutv1alpha1.ResourceMatch) []workload.Interface {
 	if match == nil || (match.Selector == nil && len(match.Names) == 0) {
@@ -91,65 +36,6 @@ func filterWorkloadsByMatch(workloads []workload.Interface, match *rolloutv1alph
 		info := w.GetInfo()
 		if macher.Matches(info.Cluster, info.Name, info.Labels) {
 			result = append(result, w)
-		}
-	}
-	return result
-}
-
-func convertFastBatch(fastBatch *rolloutv1alpha1.FastBatch) []rolloutv1alpha1.RolloutStep {
-	result := make([]rolloutv1alpha1.RolloutStep, 0)
-
-	batchCount := fastBatch.Count
-	per := int(math.Floor(float64(100) / float64(batchCount)))
-	if per < 1 {
-		per = 1
-	}
-
-	pausedBatches := getPausedBatches(batchCount, fastBatch.PausedBatches, fastBatch.PauseMode)
-
-	if fastBatch.Beta != nil {
-		result = append(result, rolloutv1alpha1.RolloutStep{
-			Pause:    pointer.Bool(true),
-			Replicas: *fastBatch.Beta.Replicas,
-		})
-	}
-
-	// batch index starts from 1 to batchCount
-	for i := int32(1); i <= batchCount; i++ {
-		var replicas intstr.IntOrString
-		if i == batchCount {
-			replicas = intstr.FromString("100%")
-		} else {
-			replicas = intstr.FromString(fmt.Sprintf("%d%s", int(i)*per, "%"))
-		}
-
-		step := rolloutv1alpha1.RolloutStep{
-			Replicas: replicas,
-		}
-		if pausedBatches.Has(i) {
-			step.Pause = pointer.Bool(true)
-		}
-		result = append(result, step)
-	}
-	return result
-}
-
-func getPausedBatches(count int32, pausePoint []int32, strategy rolloutv1alpha1.PauseModeType) sets.Int32 {
-	result := sets.NewInt32()
-	if count < 1 {
-		return result
-	}
-	for _, v := range pausePoint {
-		if v > 0 && v < count {
-			result.Insert(v)
-		}
-	}
-	switch strategy {
-	case rolloutv1alpha1.PauseModeTypeFirstBatch:
-		result.Insert(1)
-	case rolloutv1alpha1.PauseModeTypeEachBatch:
-		for i := int32(1); i < count; i++ {
-			result.Insert(i)
 		}
 	}
 	return result
@@ -194,9 +80,6 @@ func constructRolloutRunBatches(strategy *rolloutv1alpha1.BatchStrategy, workloa
 		return nil
 	}
 	batches := strategy.Batches
-	if strategy.FastBatch != nil {
-		batches = convertFastBatch(strategy.FastBatch)
-	}
 
 	if len(batches) == 0 {
 		panic("no valid batches found in strategy")
