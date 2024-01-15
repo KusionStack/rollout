c2ecb6f6da3f61a552536b7059d43910a6a28043
zoumo <jim.zoumo@gmail.com>
2024-01-15 10:27:58 +0800

    feat: add traffic route api types (#25)
    * feat: add traffic route types
    
    * feat: update traffic route api
    
    * feat: update traffic api

--- apis/rollout/v1alpha1/rollout_gateway_api.go
+++ apis/rollout/v1alpha1/rollout_gateway_api.go
@@ -16,170 +16,46 @@
 
 package v1alpha1
 
-// HeaderMatchType specifies the semantics of how HTTP header values should be
-// compared. Valid HeaderMatchType values are:
-//
-// * "Exact"
-// * "RegularExpression"
-//
-// +kubebuilder:validation:Enum=Exact;RegularExpression
-type HeaderMatchType string
-
-// HeaderMatchType constants.
-const (
-	HeaderMatchExact             HeaderMatchType = "Exact"
-	HeaderMatchRegularExpression HeaderMatchType = "RegularExpression"
+import (
+	gatewayapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
-// HTTPHeaderName is the name of an HTTP header.
-//
-// Valid values include:
-//
-// * "Authorization"
-// * "Set-Cookie"
-//
-// Invalid values include:
-//
-//   - ":method" - ":" is an invalid character. This means that HTTP/2 pseudo
-//     headers are not currently supported by this type.
-//   - "/invalid" - "/" is an invalid character
-//
-// +kubebuilder:validation:MinLength=1
-// +kubebuilder:validation:MaxLength=256
-// +kubebuilder:validation:Pattern=`^[A-Za-z0-9!#$%&'*+\-.^_\x60|~]+$`
-type HTTPHeaderName string
-
-// HTTPHeaderMatch describes how to select a HTTP route by matching HTTP request
-// headers.
-type HTTPHeaderMatch struct {
-	// Type specifies how to match against the value of the header.
+type HTTPRouteMatch struct {
+	// Headers specifies HTTP request header matchers. Multiple match values are
+	// ANDed together, meaning, a request must match all the specified headers
+	// to select the route.
 	//
-	// Support: Core (Exact)
-	//
-	// Support: Custom (RegularExpression)
-	//
-	// Since RegularExpression HeaderMatchType has custom conformance,
-	// implementations can support POSIX, PCRE or any other dialects of regular
-	// expressions. Please read the implementation's documentation to determine
-	// the supported dialect.
-	//
-	// +optional
-	// +kubebuilder:default=Exact
-	Type *HeaderMatchType `json:"type,omitempty"`
-
-	// Name is the name of the HTTP Header to be matched. Name matching MUST be
-	// case insensitive. (See https://tools.ietf.org/html/rfc7230#section-3.2).
-	//
-	// If multiple entries specify equivalent header names, only the first
-	// entry with an equivalent name MUST be considered for a match. Subsequent
-	// entries with an equivalent header name MUST be ignored. Due to the
-	// case-insensitivity of header names, "foo" and "Foo" are considered
-	// equivalent.
-	//
-	// When a header is repeated in an HTTP request, it is
-	// implementation-specific behavior as to how this is represented.
-	// Generally, proxies should follow the guidance from the RFC:
-	// https://www.rfc-editor.org/rfc/rfc7230.html#section-3.2.2 regarding
-	// processing a repeated header, with special handling for "Set-Cookie".
-	Name HTTPHeaderName `json:"name"`
-
-	// Value is the value of HTTP Header to be matched.
-	//
-	// +kubebuilder:validation:MinLength=1
-	// +kubebuilder:validation:MaxLength=4096
-	Value string `json:"value"`
-}
-
-// HTTPHeaderFilter defines a filter that modifies the headers of an HTTP
-// request or response. Only one action for a given header name is permitted.
-// Filters specifying multiple actions of the same or different type for any one
-// header name are invalid and will be rejected by the webhook if installed.
-// Configuration to set or add multiple values for a header must use RFC 7230
-// header value formatting, separating each value with a comma.
-type HTTPHeaderFilter struct {
-	// Set overwrites the request with the given header (name, value)
-	// before the action.
-	//
-	// Input:
-	//   GET /foo HTTP/1.1
-	//   my-header: foo
-	//
-	// Config:
-	//   set:
-	//   - name: "my-header"
-	//     value: "bar"
-	//
-	// Output:
-	//   GET /foo HTTP/1.1
-	//   my-header: bar
-	//
-	// +optional
 	// +listType=map
 	// +listMapKey=name
-	// +kubebuilder:validation:MaxItems=16
-	Set []HTTPHeader `json:"set,omitempty"`
-
-	// Add adds the given header(s) (name, value) to the request
-	// before the action. It appends to any existing values associated
-	// with the header name.
-	//
-	// Input:
-	//   GET /foo HTTP/1.1
-	//   my-header: foo
-	//
-	// Config:
-	//   add:
-	//   - name: "my-header"
-	//     value: "bar,baz"
-	//
-	// Output:
-	//   GET /foo HTTP/1.1
-	//   my-header: foo,bar,baz
-	//
 	// +optional
+	// +kubebuilder:validation:MaxItems=16
+	Headers []gatewayapiv1.HTTPHeaderMatch `json:"headers,omitempty"`
+	// QueryParams specifies HTTP query parameter matchers. Multiple match
+	// values are ANDed together, meaning, a request must match all the
+	// specified query parameters to select the route.
+	//
+	// Support: Extended
+	//
 	// +listType=map
 	// +listMapKey=name
-	// +kubebuilder:validation:MaxItems=16
-	Add []HTTPHeader `json:"add,omitempty"`
-
-	// Remove the given header(s) from the HTTP request before the action. The
-	// value of Remove is a list of HTTP header names. Note that the header
-	// names are case-insensitive (see
-	// https://datatracker.ietf.org/doc/html/rfc2616#section-4.2).
-	//
-	// Input:
-	//   GET /foo HTTP/1.1
-	//   my-header1: foo
-	//   my-header2: bar
-	//   my-header3: baz
-	//
-	// Config:
-	//   remove: ["my-header1", "my-header3"]
-	//
-	// Output:
-	//   GET /foo HTTP/1.1
-	//   my-header2: bar
-	//
 	// +optional
 	// +kubebuilder:validation:MaxItems=16
-	Remove []string `json:"remove,omitempty"`
+	QueryParams []gatewayapiv1.HTTPQueryParamMatch `json:"queryParams,omitempty"`
 }
 
-// HTTPHeader represents an HTTP Header name and value as defined by RFC 7230.
-type HTTPHeader struct {
-	// Name is the name of the HTTP Header to be matched. Name matching MUST be
-	// case insensitive. (See https://tools.ietf.org/html/rfc7230#section-3.2).
+type HTTPRouteFilter struct {
+	// RequestHeaderModifier defines a schema for a filter that modifies request
+	// headers.
 	//
-	// If multiple entries specify equivalent header names, the first entry with
-	// an equivalent name MUST be considered for a match. Subsequent entries
-	// with an equivalent header name MUST be ignored. Due to the
-	// case-insensitivity of header names, "foo" and "Foo" are considered
-	// equivalent.
-	Name HTTPHeaderName `json:"name"`
+	// Support: Core
+	//
+	// +optional
+	RequestHeaderModifier *gatewayapiv1.HTTPHeaderFilter `json:"requestHeaderModifier,omitempty"`
+}
 
-	// Value is the value of HTTP Header to be matched.
-	//
-	// +kubebuilder:validation:MinLength=1
-	// +kubebuilder:validation:MaxLength=4096
-	Value string `json:"value"`
+type HTTPRouteRule struct {
+	// Matches define conditions used for matching the incoming HTTP requests to canary service.
+	Matches []HTTPRouteMatch `json:"matches,omitempty"`
+	// Filter defines a filter for the canary service.
+	Filter HTTPRouteFilter `json:"filter,omitempty"`
 }
--- apis/rollout/v1alpha1/rollout_types.go
+++ apis/rollout/v1alpha1/rollout_types.go
@@ -60,6 +60,10 @@ type RolloutSpec struct {
 
 	// WorkloadRef is a reference to a kind of workloads
 	WorkloadRef WorkloadRef `json:"workloadRef,omitempty"`
+
+	// TrafficTopologies defines the networking traffic relationships between
+	// workloads, backend services, and routes.
+	TrafficTopologyRefs []string `json:"trafficTopologyRefs"`
 }
 
 type RolloutTriggerPolicy string
@@ -84,37 +88,6 @@ type WorkloadRef struct {
 	Kind string `json:"kind"`
 	// Match indicates how to match workloads. only one workload should be matches in one cluster
 	Match ResourceMatch `json:"match"`
-}
-
-type ResourceMatch struct {
-	// Selector is a label query over a set of resources, in this case resource
-	Selector *metav1.LabelSelector `json:"selector,omitempty"`
-	// Names is a list of workload name
-	Names []CrossClusterObjectNameReference `json:"names,omitempty"`
-}
-
-const (
-	MatchAllCluster = ""
-)
-
-// CrossClusterObjectNameReference contains cluster and name reference to a k8s object
-type CrossClusterObjectNameReference struct {
-	// Cluster indicates the name of cluster
-	Cluster string `json:"cluster,omitempty"`
-	// Name is the resource name
-	Name string `json:"name,omitempty"`
-}
-
-func (r CrossClusterObjectNameReference) Matches(cluster, name string) bool {
-	if r.Name != name {
-		// object name is not matched
-		return false
-	}
-	if r.Cluster == MatchAllCluster || cluster == MatchAllCluster {
-		// match all clusters
-		return true
-	}
-	return r.Cluster == cluster
 }
 
 // RolloutStatus defines the observed state of Rollout
@@ -236,14 +209,3 @@ const (
 	// BatchStepStateError means the step is error.
 	BatchStepStateError RolloutBatchStepState = "Error"
 )
-
-type CodeReasonMessage struct {
-	// Code is a globally unique identifier
-	Code string `json:"code,omitempty"`
-	// A human-readable short word
-	// +optional
-	Reason string `json:"reason,omitempty"`
-	// A human-readable message indicating details about the transition.
-	// +optional
-	Message string `json:"message,omitempty"`
-}
