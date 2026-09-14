57b1f17858b4d1625e848b65b5bbacf1d3240ab6
youngLiuHY <qyliu1996@163.com>
2026-09-14 14:18:21 +0800

    fix: check updated ready condition

--- pkg/workload/info.go
+++ pkg/workload/info.go
@@ -112,7 +112,7 @@ func (o *Info) CheckUpdatedReady(replicas int32, strictCheck bool) (bool, string
 		return false, "workload updated available replicas is not satisfied"
 	}
 
-	if strictCheck && o.Status.ObservedReplicas > o.Status.DesiredReplicas || o.Status.TerminatingReplicas != 0 {
+	if strictCheck && (o.Status.ObservedReplicas > o.Status.DesiredReplicas || o.Status.TerminatingReplicas != 0) {
 		return false, "workload observed replicas is more than desiredReplicas"
 	}
 	return true, ""
